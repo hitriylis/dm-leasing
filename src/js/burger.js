8c200fd3da4554330de49037ef31673132b5c486
hitriylis <avlugovov@gmail.com>
2024-10-06 15:11:59 +0400

    update burger menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,5 +2,8 @@ const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
     menu.classList.toggle('menu__list--active');
 });
+
+
